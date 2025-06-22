a4ba55707b657b8e0a80dc65c37b0136d2f68a6c
Gabriel Duarte Knevitz Oliveira <gabrielknevitzoliveira@gmail.com>
2025-06-22 19:51:05 -0300

    Prova Programação Java

---
+++ src/Agendamento.java
@@ -0,0 +1,13 @@
+//QUESTÃO NÚMERO 1
+/*import java.time.LocalDate;
+
+public class Agendamento {
+    public static void main(String[] args) {
+        LocalDate base = LocalDate.of(2025, 6, 1); // 1º de junho de 2025
+        int dias = 7;
+        LocalDate novaData = base.plusDays(dias * 2 - 3);
+        System.out.println(novaData.getDayOfMonth());
+    }
+}
+//RESPOSTA DE SAÍDA: 12    
+*/
